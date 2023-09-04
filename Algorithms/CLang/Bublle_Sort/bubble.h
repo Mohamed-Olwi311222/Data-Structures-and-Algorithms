01a0621e367fc0be0b33a8b0401d6922225929b1
AceMido <acemido2004@gmail.com>
2023-09-04 05:52:59 +0300

    bubble sort

---
+++ Algorithms/CLang/Bublle_Sort/bubble.h
@@ -0,0 +1,10 @@
+#ifndef BUBBLE_H
+#define BUBBLE_H
+/*---------------------------Include Files------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/*---------------------------Function prototypes-------------------------*/
+bool bubble_sort(int *arr, int size);
+void print_arr(int *arr, int size);
+#endif /*BUBBLE_H*/
