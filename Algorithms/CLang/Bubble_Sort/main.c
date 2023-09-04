32606959c1649f435b8e96ee361e92a55f82871d
AceMido <acemido2004@gmail.com>
2023-09-04 11:08:05 +0300

    optimized erorr handling in both sorts and fixed Bubble_Sort dir name and added selection sort

---
+++ Algorithms/CLang/Bubble_Sort/main.c
@@ -0,0 +1,11 @@
+#include "bubble.h"
+
+int main (void)
+{
+    int arr[6] = {1, 4, 2, 5 ,0, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    print_arr(arr, size);
+    bubble_sort(arr, size);
+    print_arr(arr, size);
+}
